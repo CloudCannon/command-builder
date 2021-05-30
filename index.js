26894b7708885f49d24d43ceb73cb9e234088b65
George Phillips <Georgepaulphillips@gmail.com>
2021-05-31 11:59:37 +1200

    Export Eleventy for external use

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const Hugo = require('./lib/hugo');
 const Gatsby = require('./lib/gatsby');
 const Rosey = require('./lib/rosey');
 const Jekyll = require('./lib/jekyll');
+const Eleventy = require('./lib/eleventy');
 const Reseed = require('./lib/reseed');
 const Parser = require('./helpers/parser');
 const BuildOptions = require('./options/ssg-options');
@@ -12,6 +13,7 @@ module.exports = {
 	Hugo: Hugo,
 	Gatsby: Gatsby,
 	Rosey: Rosey,
+	Eleventy: Eleventy,
 	Jekyll: Jekyll,
 	Reseed: Reseed,
 	Parser: Parser,
