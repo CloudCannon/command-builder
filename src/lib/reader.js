912a7b206b8f3ec0ec7f3c71630badd02405adcd
Tate <tate@cloudcannon.com>
2023-04-18 09:58:20 +1200

    Reset directory after custom commands

--- src/lib/reader.js
+++ src/lib/reader.js
@@ -4,10 +4,10 @@ const { addEchoCommand } = require('../helpers/commands');
 
 function getCheckCommands() {
 	return [
-		'DETECTED_NPM_VERSION=$((npm -v 2> /dev/null || echo \'unknown\') | sed "s/[][]//g")',
+		"DETECTED_NPM_VERSION=$((npm -v 2> /dev/null || echo 'unknown') | sed \"s/[][]//g\")",
 		'DETECTED_NODE_VERSION=$((node -v 2> /dev/null || echo \'unknown\') | sed "s/[][]//g" | sed "s/^v//")',
 		'DETECTED_DENO_VERSION=$((deno -V 2> /dev/null || echo \'unknown\') | sed "s/[][]//g" | sed "s/^deno //")',
-		'DETECTED_YARN_VERSION=$(yarn -v 2> /dev/null || echo \'unknown\')',
+		"DETECTED_YARN_VERSION=$(yarn -v 2> /dev/null || echo 'unknown')",
 		'DETECTED_BUNDLE_VERSION=$((bundle -v 2> /dev/null || echo \'unknown\') | sed "s/[][]//g" | sed "s/^Bundler version //g")',
 		'DETECTED_RUBY_VERSION=$((ruby -v 2> /dev/null || echo \'unknown\') | sed "s/[][]//g" | sed "s/^ruby //g" | cut -d " " -f 1)',
 
@@ -23,14 +23,18 @@ function getCheckCommands() {
 }
 
 function getInstallCommands(buildConfig) {
-	return buildConfig.install_command ? [buildConfig.install_command] : [];
+	return buildConfig.install_command
+		? [buildConfig.install_command, `cd ${Compiler.getInputPath()}`]
+		: [];
 }
 
 function getBuildCommands(buildConfig, buildOutputPath) {
 	const pluginTag = buildConfig.use_beta_plugin ? '@next' : '@latest';
 
 	return [
-		...(buildConfig.build_command ? [buildConfig.build_command] : []),
+		...(buildConfig.build_command
+			? [buildConfig.build_command, `cd ${Compiler.getInputPath()}`]
+			: []),
 		`npx @cloudcannon/reader${pluginTag} --output "${buildOutputPath}"`
 	];
 }
@@ -44,9 +48,15 @@ module.exports = class Reader {
 			...getInstallCommands(buildConfig).reduce(addEchoCommand, []),
 			...Compiler.getPrebuildCommands(),
 			...getCheckCommands(),
-			...getBuildCommands(buildConfig, buildOutputPath).reduce(addEchoCommand, []),
+			...getBuildCommands(buildConfig, buildOutputPath).reduce(
+				addEchoCommand,
+				[]
+			),
 			...Compiler.getPostbuildCommands(),
-			...Compiler.getOutputCommands(buildOutputPath, buildConfig.preserveOutput),
+			...Compiler.getOutputCommands(
+				buildOutputPath,
+				buildConfig.preserveOutput
+			),
 			...Compiler.getExportCommands()
 		];
 	}
