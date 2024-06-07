4e473891ed21abef438258fe1b81f423c52b65e7
Tate <tate@cloudcannon.com>
2024-06-07 12:49:50 +1200

    Default static output path to dot

--- src/lib/static.js
+++ src/lib/static.js
@@ -4,7 +4,7 @@ module.exports = class Static {
 	static runScriptCommands(buildConfig = {}) {
 		return Reader.runScriptCommands({
 			...buildConfig,
-			output_path: buildConfig.source || '',
+			output_path: buildConfig.source || '.',
 			install_command: '',
 			build_command: ''
 		});
